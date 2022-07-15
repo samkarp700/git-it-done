a649532d6fe3741adaf62e2b75c3d3e9b3c02289
Sam Karp <sfcypret@gmail.com>
2022-07-15 15:44:19 -0400

    added search languages tool

--- assets/css/js/homepage.js
+++ assets/css/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 
 var getUserRepos = function(user) {
@@ -48,8 +49,6 @@ var displayRepos = function(repos, searchTerm) {
         return;
     }
 
-    console.log(repos);
-    console.log(searchTerm);
     // clear old content
     repoContainerEl.textContent="";
     repoSearchTerm.textContent= searchTerm;
@@ -92,5 +91,33 @@ var displayRepos = function(repos, searchTerm) {
     }
 }
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+                
+            });
+          
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    })
+}
+
+var buttonClickHandler = function(event) {
+    if (language) {
+        getFeaturedRepos(language);
+    // clear old conten
+    repoContainerEl.textContent="";
+    }
+
+    var language = event.target.getAttribute("data-language");
+    
+    getFeaturedRepos(language);
+}
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
